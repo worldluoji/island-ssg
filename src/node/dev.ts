2eea2b49fa82b49c7ff624329b3908e5d51e9752
qq1132416 <qq1132416@outlook.com>
2023-01-03 16:12:09 +0800

    feat: 增加配置文件解析

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -3,8 +3,12 @@ import { pluginIndexHtml } from './plugin-island/indexHtml';
 import pluginReact from '@vitejs/plugin-react';
 import { PACKAGE_ROOT } from './constants';
 
+import { resolveConfig } from './config';
+
 // 使用 vite的 Dev Server
 export async function createDevServer(root = process.cwd()) {
+  const config = await resolveConfig(root, 'serve', 'development');
+  console.log(config);
   return createViteDevServer({
     root,
     plugins: [pluginIndexHtml(), pluginReact()],
